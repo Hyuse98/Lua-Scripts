32cd88e86f79acacbcc480b731de4d8319243226
Franklin <franklinpereira98@hotmail.com>
2025-04-09 18:35:40 -0300

    Add Function modify_paired_bones

--- GI/PairBoneModifier.lua
+++ GI/PairBoneModifier.lua
@@ -51,3 +51,69 @@ local function find_body(avatar)
     end
     return avatar
 end
+
+local function modify_paired_bones()
+    local avatar = find_active_char()
+    if not avatar then
+        CS.MoleMole.ActorUtils.ShowMessage("Active Char not Found"); return
+    end
+
+    local body = find_body(avatar)
+    if not body then
+        CS.MoleMole.ActorUtils.ShowMessage("Body not Found")
+    end
+
+    local renderer = body:GetComponent("SkinnedMeshRenderer")
+
+    if not (renderer and renderer.bones) then
+        CS.MoleMole.ActorUtils.ShowMessage("ERROR: Mesh Renderer not Found!!!"); return
+    end
+
+    local bones = renderer.bones
+    local container_transform = nil
+
+    local existing_container = avatar.transform:Find(CONTAINER_NAME, true)
+
+    if existing_container then
+        container_transform = existing_container
+        CS.MoleMole.ActorUtils.ShowMessage("Container '" .. CONTAINER_NAME .. "' Already in Use")
+    else
+        CS.MoleMole.ActorUtils.ShowMessage("Creating container '" .. CONTAINER_NAME .. "'...")
+        local bone_l, bone_r = nil, nil
+
+        for i = 0, bones.Length - 1 do
+            if bones[i].name == BONE_NAME_L then bone_l = bones[i] end
+            if bones[i].name == BONE_NAME_R then bone_r = bones[i] end
+            if bone_l and bone_r then break end
+        end
+
+        if not (bone_l and bone_r) then
+            CS.MoleMole.ActorUtils.ShowMessage("ERRO: Bone '" .. BONE_NAME_L .. "' or '" .. BONE_NAME_R .. "' not Found.")
+            return
+        end
+
+        local shared_parent = bone_l.transform.parent
+        if shared_parent ~= bone_r.transform.parent then
+            CS.MoleMole.ActorUtils.ShowMessage(
+                "WARNING: Bones L and R dont share same parent, Using bone L parent.")
+        end
+
+        local container_go = CS.UnityEngine.GameObject(CONTAINER_NAME)
+
+        container_transform = container_go.transform
+
+        container_transform.parent = shared_parent
+
+        container_transform.localPosition = CS.UnityEngine.Vector3.zero
+        container_transform.localRotation = CS.UnityEngine.Quaternion.identity
+        container_transform.localScale = CS.UnityEngine.Vector3.one
+
+        bone_l.transform.parent = container_transform
+        bone_r.transform.parent = container_transform
+    end
+
+    if container_transform then
+        container_transform.localScale = CS.UnityEngine.Vector3(NEW_SCALE_X, NEW_SCALE_Y, NEW_SCALE_Z)
+        CS.MoleMole.ActorUtils.ShowMessage("Scaling sucessfuly on '" .. CONTAINER_NAME .. "'.")
+    end
+end
