379bb85a0a15bc93fb4c375f67dd8e9514ddf81a
Franklin <franklinpereira98@hotmail.com>
2025-05-25 20:25:56 -0300

    Add single mode to left and right(can be used to a center bone too)

--- GI/PairBoneModifier.lua
+++ GI/PairBoneModifier.lua
@@ -1,21 +1,38 @@
 -- ===============================================================
--- VARIABLES
+-- CONFIGURATION VARIABLES
 -- ===============================================================
 
+-- Target Bone Names
 local BONE_NAME_L = "+Breast L A01"    -- Left
 local BONE_NAME_R = "+Breast R A01"    -- Right
 
-local CONTAINER_NAME = "Breast_Scaler" -- Container Name, Change if u applying new modifications on same char
+-- ---------------------------------------------------------------
+-- MODE SELECTION: Choose only ONE option as 'true'
+-- ---------------------------------------------------------------
+-- Priority order is: Left -> Right -> Pair.
+-- If MODIFY_LEFT_ONLY is true, it will only modify the left side, ignoring the others.
 
--- Basic Math
--- 2.0 Double - Twice Actual Size
--- 1.5 Half - 50% Bigger
--- 1.0 Default - No Changes
--- 0.5 Half - 50% Smaller
+local MODIFY_PAIR = true       -- Modifies BOTH bones (L and R) with the same settings below.
+local MODIFY_LEFT_ONLY = false -- Modifies ONLY the left bone (BONE_NAME_L).
+local MODIFY_RIGHT_ONLY = false-- Modifies ONLY the right bone (BONE_NAME_R).
 
-local NEW_SCALE_X = 1.2
-local NEW_SCALE_Y = 1.2
-local NEW_SCALE_Z = 1.2
+-- ---------------------------------------------------------------
+-- TRANSFORMATION SETTINGS
+-- (Will be applied to the bones selected by the mode above)
+-- ---------------------------------------------------------------
+
+-- Suffix for the container name. The final name will be "BoneName" .. SUFFIX.
+-- Example: "+Breast L A01_ModContainer"
+local CONTAINER_SUFFIX = "_ModContainer"
+
+local APPLY_SCALE = false
+local NEW_SCALE = { x = 1.1, y = 1.1, z = 1.1 }
+
+local APPLY_ROTATION = false
+local OFFSET_ROT = { x = 0, y = 0, z = 0 }
+
+local APPLY_POSITION = true
+local OFFSET_POS = {x = 0, y = -0.001, z = 0}
 
 -- ===============================================================
 -- SCRIPT
@@ -52,74 +69,85 @@ local function find_body(avatar)
     return avatar
 end
 
-local function modify_paired_bones()
-    local avatar = find_active_char()
-    if not avatar then
-        CS.MoleMole.ActorUtils.ShowMessage("Active Char not Found"); return
+local function modify_single_bone(all_bones, bone_name)
+    local target_bone = nil
+    for i = 0, all_bones.Length - 1 do
+        if all_bones[i].name == bone_name then
+            target_bone = all_bones[i]
+            break
+        end
     end
 
-    local body = find_body(avatar)
-    if not body then
-        CS.MoleMole.ActorUtils.ShowMessage("Body not Found")
+    if not target_bone then
+        CS.MoleMole.ActorUtils.ShowMessage("ERROR: Bone '" .. bone_name .. "' not found.")
+        return
     end
 
-    local renderer = body:GetComponent("SkinnedMeshRenderer")
+    local container_name = bone_name .. CONTAINER_SUFFIX
+    local bone_parent = target_bone.transform.parent
+    local container_transform = bone_parent:Find(container_name)
 
-    if not (renderer and renderer.bones) then
-        CS.MoleMole.ActorUtils.ShowMessage("ERROR: Mesh Renderer not Found!!!"); return
-    end
-
-    local bones = renderer.bones
-    local container_transform = nil
-
-    local existing_container = avatar.transform:Find(CONTAINER_NAME, true)
-
-    if existing_container then
-        container_transform = existing_container
-        CS.MoleMole.ActorUtils.ShowMessage("Container '" .. CONTAINER_NAME .. "' Already in Use")
-    else
-        CS.MoleMole.ActorUtils.ShowMessage("Creating container '" .. CONTAINER_NAME .. "'...")
-        local bone_l, bone_r = nil, nil
-
-        for i = 0, bones.Length - 1 do
-            if bones[i].name == BONE_NAME_L then bone_l = bones[i] end
-            if bones[i].name == BONE_NAME_R then bone_r = bones[i] end
-            if bone_l and bone_r then break end
-        end
-
-        if not (bone_l and bone_r) then
-            CS.MoleMole.ActorUtils.ShowMessage("ERRO: Bone '" .. BONE_NAME_L .. "' or '" .. BONE_NAME_R .. "' not Found.")
-            return
-        end
-
-        local shared_parent = bone_l.transform.parent
-        if shared_parent ~= bone_r.transform.parent then
-            CS.MoleMole.ActorUtils.ShowMessage(
-                "WARNING: Bones L and R dont share same parent, Using bone L parent.")
-        end
-
-        local container_go = CS.UnityEngine.GameObject(CONTAINER_NAME)
-
+    if not container_transform then
+        CS.MoleMole.ActorUtils.ShowMessage("Creating container for '" .. bone_name .. "'...")
+        local container_go = CS.UnityEngine.GameObject(container_name)
         container_transform = container_go.transform
-
-        container_transform.parent = shared_parent
+        container_transform.parent = bone_parent
 
         container_transform.localPosition = CS.UnityEngine.Vector3.zero
         container_transform.localRotation = CS.UnityEngine.Quaternion.identity
         container_transform.localScale = CS.UnityEngine.Vector3.one
 
-        bone_l.transform.parent = container_transform
-        bone_r.transform.parent = container_transform
+        target_bone.transform.parent = container_transform
+    else
+        CS.MoleMole.ActorUtils.ShowMessage("Container for '" .. bone_name .. "' already exists. Reusing.")
     end
 
-    if container_transform then
-        container_transform.localScale = CS.UnityEngine.Vector3(NEW_SCALE_X, NEW_SCALE_Y, NEW_SCALE_Z)
-        CS.MoleMole.ActorUtils.ShowMessage("Scaling sucessfuly on '" .. CONTAINER_NAME .. "'.")
+    if APPLY_SCALE then
+        container_transform.localScale = CS.UnityEngine.Vector3(NEW_SCALE.x, NEW_SCALE.y, NEW_SCALE.z)
     end
+    if APPLY_ROTATION then
+        container_transform.localEulerAngles = CS.UnityEngine.Vector3(OFFSET_ROT.x, OFFSET_ROT.y, OFFSET_ROT.z)
+    end
+    if APPLY_POSITION then
+        container_transform.localPosition = CS.UnityEngine.Vector3(OFFSET_POS.x, OFFSET_POS.y, OFFSET_POS.z)
+    end
+end
+
+local function apply_bone_mods()
+    local avatar = find_active_char()
+    if not avatar then CS.MoleMole.ActorUtils.ShowMessage("Active character not found."); return end
+
+    local body = find_body(avatar)
+    if not body then CS.MoleMole.ActorUtils.ShowMessage("Body not found."); return end
+
+    local renderer = body:GetComponent("SkinnedMeshRenderer")
+    if not (renderer and renderer.bones) then
+        CS.MoleMole.ActorUtils.ShowMessage("ERROR: Mesh Renderer not found!")
+        return
+    end
+
+    local all_bones = renderer.bones
+
+    if MODIFY_LEFT_ONLY then
+        CS.MoleMole.ActorUtils.ShowMessage("Mode: Left Only")
+        modify_single_bone(all_bones, BONE_NAME_L)
+    elseif MODIFY_RIGHT_ONLY then
+        CS.MoleMole.ActorUtils.ShowMessage("Mode: Right Only")
+        modify_single_bone(all_bones, BONE_NAME_R)
+    elseif MODIFY_PAIR then
+        CS.MoleMole.ActorUtils.ShowMessage("Mode: Pair")
+        modify_single_bone(all_bones, BONE_NAME_L)
+        modify_single_bone(all_bones, BONE_NAME_R)
+    else
+        CS.MoleMole.ActorUtils.ShowMessage("No modification mode selected.")
+        return
+    end
+
+    CS.MoleMole.ActorUtils.ShowMessage("Bone modification completed!")
 end
 
 local function onError(error)
-    CS.MoleMole.ActorUtils.ShowMessage("ERRO: Unexpected Error\n" .. tostring(error))
+    CS.MoleMole.ActorUtils.ShowMessage("UNEXPECTED ERROR:\n" .. tostring(error))
 end
 
-xpcall(modify_paired_bones, onError)
+xpcall(apply_bone_mods, onError)
